17e4abae4f48e55f05d6275ea69af1b062dac7bd
Ledowsky <kinolenta@yahoo.com>
2017-09-06 15:32:03 +0300

    File .gitignore updated with additional parameters. File HeloWorld.java created.

---
+++ src/com/levelp/jb2/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.levelp.jb2;
+
+/**
+ * Created by DELL on 06.09.2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!!!");
+    }
+}
