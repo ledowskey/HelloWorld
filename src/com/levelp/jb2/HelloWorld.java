753178bf816dde57cabb2b6a150527db54935ee1
Ledowsky <kinolenta@yahoo.com>
2017-09-07 09:56:09 +0300

    File .gitignore updated with additional parameters. File HeloWorld.java created. Zip archive created

--- src/com/levelp/jb2/HelloWorld.java
+++ src/com/levelp/jb2/HelloWorld.java
@@ -1,10 +1,26 @@
 package com.levelp.jb2;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 /**
- * Created by DELL on 06.09.2017.
+ * Created by Ledowsky on 06.09.2017.
  */
+
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("HelloWorld!!!");
+
+        ZipOutputStream zip = new ZipOutputStream(new FileOutputStream("C:\\JavaRush\\archive.zip"));
+
+        zip.putNextEntry(new ZipEntry("document.txt"));
+
+        File file = new File("C:\\JavaRush\\1.txt");
+        Files.copy(file.toPath(),zip);
+        System.out.println("Operation successful!");
     }
 }
